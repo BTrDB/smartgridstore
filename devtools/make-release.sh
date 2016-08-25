0feae53de1b43635da71eff106d5454a601d1d6e
Michael Andersen <Michael Andersen>
2016-08-25 12:41:06 -0700

    Add some missing stuff

---
+++ devtools/make-release.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+if [[ "$(basename $PWD)" = "devtools" ]]
+then
+  cd ..
+fi
+mkdir smartgridstore
+rsync -a bin smartgridstore/
+rsync -a templates smartgridstore/
+mkdir smartgridstore/units
+rsync -a cluster-info.sh smartgridstore/
+rsync -a README.md smartgridstore/
+rsync -a LICENSE smartgridstore/
+tar cvf 3.4.tar smartgridstore
+gzip -9 3.4.tar
