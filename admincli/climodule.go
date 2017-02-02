e403b2430ba6d81dd1741a08c35d4f92b814645f
Michael Andersen <m.andersen@cs.berkeley.edu>
2017-02-01 18:25:16 -0800

    Changed interface

--- admincli/climodule.go
+++ admincli/climodule.go
@@ -1,8 +1,14 @@
 package admincli
 
-import "context"
+import (
+	"context"
+	"io"
+)
+
+type key int
 
 const MaxHintLength = 50
+const ConsoleWidth key = 0
 
 type CLIModule interface {
 	//The children under this module. Invalid if runnable = true
@@ -23,15 +29,10 @@ type CLIModule interface {
 	//Is this a command? false if it is a category
 	Runnable() bool
 
-	//Run this module. The output channel will write
-	//to the user. If argsOk is not true, output will be ignored
-	//and the Usage() string will be sent to the user. For more
-	//complex argument problems (nonexistent object for example),
-	//return argsOk true and print more detailed messages on
-	//the output. Do not close output. Return when the command is done.
+	//Run this module. Return when the command is done.
 	//Do not write to output after returning
 	//The context will contain the final window width as well.
 	//ctx.Value(adminCli.ConsoleWidth) will be an integer. -1 if unknown
 	//and >1 if the width is known
-	Run(ctx context.Context, output chan string, args ...string) (argsOk bool)
+	Run(ctx context.Context, output io.Writer, args ...string) (argsOk bool)
 }
