d8866851e121ebe513c5359ad2720dedb31b07ca
Michael Andersen <michael@steelcode.com>
2017-07-17 11:53:56 -0700

    Add etcd cluster stuff

---
+++ manifest_templates/readme.md
@@ -0,0 +1,3 @@
+# Smart Grid Store readme
+
+toto
---
+++ manifest_templates/secret_ceph_keyring.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# {{ .GenLine }}
+# this creates the ceph config secret
+set -ex
+if [ ! -e /etc/ceph/ceph.conf ]
+then
+    echo "Could not locate /etc/ceph/ceph.conf"
+    exit 1
+fi
+if [ ! -e /etc/ceph/ceph.client.admin.keyring ]
+then
+    echo "Could not locate /etc/ceph/ceph.client.admin.keyring"
+    exit 1
+fi
+pushd /etc/ceph
+kubectl create secret -n {{.TargetNamespace}} generic ceph-keyring --from-file=ceph.client.admin.keyring --from-file=ceph.conf
+popd
+
+secret=$(cat /etc/ceph/ceph.client.admin.keyring  | grep "key" | cut -d'=' -f2 | tr -d '[:space:]')
+kubectl create secret generic ceph-rbd-secret -n {{.TargetNamespace}} --type="kubernetes.io/rbd" --from-literal=key=${secret}
