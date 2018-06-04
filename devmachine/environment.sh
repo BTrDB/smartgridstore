c877ef487945ac588a18a664f753306d885882d1
Michael Andersen <m.andersen@cs.berkeley.edu>
2018-06-04 16:38:48 -0700

    Use the latest release in devmachine

--- devmachine/environment.sh
+++ devmachine/environment.sh
@@ -10,7 +10,7 @@ export OSDBASE=$DEVMACHINE_BASE/ceph
 export ETCDBASE=$DEVMACHINE_BASE/etcd
 
 # which version of BTrDB and tools to install
-export VERSION=4.10.1
+export VERSION=4.11.0
 
 # pick which branch to use
 export PREFIX=""
